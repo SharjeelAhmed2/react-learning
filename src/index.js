19f9cf930b78d75a34275fd5f2dfda61f8cca545
Sharjeel <sharjeelahmed614@gmail.com>
2021-05-29 16:51:44 -0700

    Array Mapping instead of individual writing

--- src/index.js
+++ src/index.js
@@ -5,23 +5,20 @@ import * as questions from './components/All';
 import * as calculator from './calculator/Calculator';
 import * as cards from './props/Cards';
 import CardArray from './props/CardArray';
+
+function gCard(val)
+{
+  return(
+    <cards.default
+    imgSrc={val.imgSrc}
+    title={val.title}
+    description={val.description}
+    />
+  );
+}
 ReactDOM.render(
   <>
-  <cards.default
-  imgSrc={CardArray[0].imgSrc}
-  title={CardArray[0].title}
-  description={CardArray[0].description}
-   />
-    <cards.default
-  imgSrc={CardArray[1].imgSrc}
-  title={CardArray[1].title}
-  description={CardArray[1].description}
-  />
-    <cards.default
-  imgSrc={CardArray[2].imgSrc}
-  title={CardArray[2].title}
-  description={CardArray[2].description}
-  />
+  {CardArray.map(gCard)}
   </>
 ,
   document.getElementById('root')
